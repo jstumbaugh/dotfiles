d9cecb38997796016a8714011efdbf9e699b18d6
jstumbaugh <jstumbaugh@smu.edu>
2016-09-15 13:05:26 -0500

    Update fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -17,26 +17,30 @@ function fish_prompt
   set -g __fish_git_prompt_show_informative_status true
   set -g __fish_git_prompt_showcolorhints true
 
+  set -g __fish_git_prompt_char_stateseparator ' '
+  set -g __fish_git_prompt_char_upstream_equal ''
+
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_color_dirtystate red
-  set -g __fish_git_prompt_char_dirtystate ' U:'
+  set -g __fish_git_prompt_char_dirtystate 'U:'
 
   set -g __fish_git_prompt_showstagedstate true
   set -g __fish_git_prompt_color_stagedstate red
-  set -g __fish_git_prompt_char_stagedstate ' S:'
+  set -g __fish_git_prompt_char_stagedstate 'S:'
 
   set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_char_stashstate ' $:'
+  set -g __fish_git_prompt_color_stashstate yellow
+  set -g __fish_git_prompt_char_stashstate '$:'
 
   set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_color_untrackedfiles red
-  set -g __fish_git_prompt_char_untrackedfiles ' ?:'
+  set -g __fish_git_prompt_char_untrackedfiles '?:'
 
   set -g __fish_git_prompt_showupstream verbose
   set -g __fish_git_prompt_color_upstream_ahead bgreen
-  set -g __fish_git_prompt_char_upstream_ahead ' ↑'
+  set -g __fish_git_prompt_char_upstream_ahead '↑'
   set -g __fish_git_prompt_color_upstream_behind bgreen
-  set -g __fish_git_prompt_char_upstream_behind ' ↓'
+  set -g __fish_git_prompt_char_upstream_behind '↓'
 
   # Top
   echo -n $cyan$USER$normal: $yellow(prompt_pwd | sed -e "s|^$HOME|~|")$normal
