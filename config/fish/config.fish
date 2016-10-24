6c662fcc778f24c53af29b13871f5f53cdba4db4
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2016-10-24 15:25:31 -0500

    Persist abbreviations and source plugin files

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,37 @@
+# set editor
+set -xg EDITOR vim
+
+# set no greeting
+set fish_greeting
+
+# Abbreviations
+abbr -a c clear
+
+# Git
+abbr -a ga git add
+abbr -a gap git add --patch
+abbr -a gc git commit -v
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gf git fetch
+abbr -a gl git l
+abbr -a gll git ll
+abbr -a gpl git pull
+abbr -a gp git push
+abbr -a gpo git push origin
+abbr -a gr git rebase
+abbr -a grc git rebase --continue
+abbr -a gs git status -s
+
+# rake
+abbr -a rdm rake db:migrate
+abbr -a rdr rake db:rollback
+abbr -a rdtp rake db:test:prepare
+
+# rails
+abbr -a rc rails console
+abbr -a bers bundle exec rails server -b 0.0.0.0
+
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
@@ -5,5 +39,8 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-set -xg EDITOR nvim
 
+# source each plugin file
+for file in ~/.config/fish/conf.d/*.fish
+  source $file
+end
