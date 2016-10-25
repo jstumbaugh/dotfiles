cbf365cdf9a8d00412718a15ad05bf507f56dee6
jstumbaugh <jstumbaugh@smu.edu>
2016-10-25 08:50:53 -0500

    Fix bottom of command promt error

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,3 @@
-# set editor
-set -xg EDITOR vim
-
 # set no greeting
 set fish_greeting
 
@@ -31,14 +28,6 @@ abbr -a rdtp rake db:test:prepare
 # rails
 abbr -a rc rails console
 abbr -a bers bundle exec rails server -b 0.0.0.0
-
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
 
 # source each plugin file
 for file in ~/.config/fish/conf.d/*.fish
