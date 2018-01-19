592d105ee8000f7efd8b4dfd1188af1e6210778c
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2018-01-19 11:41:38 -0600

    Move abbreviations to their own file

---
+++ config/fish/abbreviations.fish
@@ -0,0 +1,48 @@
+# Git
+abbr -a ga   git add
+abbr -a gap  git add --patch
+abbr -a gc   git commit --verbose
+abbr -a gca  git commit --verbose --amend
+abbr -a gco  git checkout
+abbr -a gd   git diff
+abbr -a gf   git fetch
+abbr -a gl   git l
+abbr -a gll  git ll
+abbr -a gp   git push
+abbr -a gpl  git pull
+abbr -a gpo  git push origin
+abbr -a gr   git rebase
+abbr -a grc  git rebase --continue
+abbr -a grn  git rebase next
+abbr -a grs  git reset
+abbr -a grsh git reset HEAD~
+abbr -a gs   git status --short
+abbr -a gst  git stash
+abbr -a gstd git stash drop
+abbr -a gstp git stash pop
+abbr -a gsts git stash show
+
+# Rake
+abbr -a berdm  bundle exec rake db:migrate
+abbr -a berdr  bundle exec rake db:rollback
+abbr -a berdtp bundle exec rake db:test:prepare
+
+# Rails
+abbr -a b    bundle
+abbr -a be   bundle exec
+abbr -a berc bundle exec rails console
+abbr -a bers bundle exec rails server -b 0.0.0.0
+
+# Tmux
+abbr -a t   tmux
+abbr -a td  tmux detach
+abbr -a tls tmux ls
+
+# ToDo
+abbr -a todo e ~/vimwiki/work/todo.md
+
+# Fuck SSH
+abbr -a fssh "eval (ssh-agent -c); and ssh-add"
+
+# Postgres Database Sizes
+abbr -a dbsize "psql postgres -c 'SELECT datname AS db_name, pg_size_pretty(pg_database_size(datname)) db_size FROM pg_database ORDER BY pg_database_size(datname) DESC;'"
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,45 +1,7 @@
 # set no greeting
 set fish_greeting
 
-# Git
-abbr -a ga   git add
-abbr -a gap  git add --patch
-abbr -a gc   git commit --verbose
-abbr -a gca  git commit --verbose --amend
-abbr -a gco  git checkout
-abbr -a gd   git diff
-abbr -a gf   git fetch
-abbr -a gl   git l
-abbr -a gll  git ll
-abbr -a gp   git push
-abbr -a gpl  git pull
-abbr -a gpo  git push origin
-abbr -a gr   git rebase
-abbr -a grc  git rebase --continue
-abbr -a grn  git rebase next
-abbr -a grs  git reset
-abbr -a grsh git reset HEAD~
-abbr -a gs   git status --short
-abbr -a gst  git stash
-abbr -a gstd git stash drop
-abbr -a gstp git stash pop
-abbr -a gsts git stash show
-
-# Rake
-abbr -a berdm  bundle exec rake db:migrate
-abbr -a berdr  bundle exec rake db:rollback
-abbr -a berdtp bundle exec rake db:test:prepare
-
-# Rails
-abbr -a b    bundle
-abbr -a be   bundle exec
-abbr -a berc bundle exec rails console
-abbr -a bers bundle exec rails server -b 0.0.0.0
-
-# Tmux
-abbr -a t   tmux
-abbr -a td  tmux detach
-abbr -a tls tmux ls
+source ~/.config/fish/abbreviations.fish
 
 # Source each plugin file
 for file in ~/.config/fish/conf.d/*.fish
@@ -58,10 +20,3 @@ set PATH $HOME/.rbenv/shims $PATH
 status --is-interactive; and source (rbenv init -|psub)
 # rbenv rehash >/dev/null ^&1
 set --export RBENV_VERSION 2.3.5
-
-# ToDo
-abbr -a todo e ~/vimwiki/work/todo.md
-
-abbr -a fssh "eval (ssh-agent -c); and ssh-add"
-
-abbr -a dbsize "psql postgres -c 'SELECT datname AS db_name, pg_size_pretty(pg_database_size(datname)) db_size FROM pg_database ORDER BY pg_database_size(datname) DESC;'"
