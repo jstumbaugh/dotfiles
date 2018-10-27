fcc5fa1f75059c848302e38e5d7d234c34ae1389
Jason Stumbaugh <stumbaughjason@gmail.com>
2018-10-27 13:04:12 -0500

    Remove trailing whitespace

--- config/fish/autostart_ssh_agent.fish
+++ config/fish/autostart_ssh_agent.fish
@@ -5,7 +5,7 @@ function start_agent
   echo "Initializing new SSH agent ..."
   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
   echo "succeeded"
-  chmod 600 $SSH_ENV 
+  chmod 600 $SSH_ENV
   . $SSH_ENV > /dev/null
   ssh-add
 end
